890c96d67f3b0c3c276a6bf365cf56a0457ddd51
Ram <ramgopal.bovilla@zopsmart.com>
2021-09-23 18:51:48 +0530

    Update import_provisioning.sh

--- provisioning/import_provisioning.sh
+++ provisioning/import_provisioning.sh
@@ -2,8 +2,8 @@
 
 # Decrypt the files
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$PROVISIONING_PASSWORD" --output provisioning/AppStoreCertificates.p12 provisioning/AppStoreCertificates.p12.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$PROVISIONING_PASSWORD" --output provisioning/CocoaHeadsNL-AppStore.mobileprovision provisioning/CocoaHeadsNL-AppStore.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$provisioning_password" --output provisioning/AppStoreCertificates.p12 provisioning/AppStoreCertificates.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$provisioning_password" --output provisioning/CocoaHeadsNL-AppStore.mobileprovision provisioning/CocoaHeadsNL-AppStore.mobileprovision.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
@@ -15,7 +15,7 @@ echo "List profiles"
 ls ~/Library/MobileDevice/Provisioning\ Profiles/
 
 security create-keychain -p "" build.keychain
-security import provisioning/AppStoreCertificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$PROVISIONING_PASSWORD" -A
+security import provisioning/AppStoreCertificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$provisioning_password" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
