3a947701f266d637f2b6779b819278a882184e0d
Ram <ramgopal.bovilla@zopsmart.com>
2021-09-23 19:44:18 +0530

    gfghfhg

---
+++ .github/secrets/import_provisioning.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Decrypt the files
+# --batch to prevent interactive command --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$provisioning_password" --output ./.github/secrets/AppStoreCertificates.p12 ./.github/secrets/AppStoreCertificates.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$provisioning_password" --output ./.github/secrets/CocoaHeadsNL-AppStore.mobileprovision ./.github/secrets/CocoaHeadsNL-AppStore.mobileprovision.gpg
+
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+
+echo "List profiles"
+ls ~/Library/MobileDevice/Provisioning\ Profiles/
+echo "Move profiles"
+cp provisioning/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+echo "List profiles"
+ls ~/Library/MobileDevice/Provisioning\ Profiles/
+
+security create-keychain -p "" build.keychain
+security import provisioning/AppStoreCertificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$provisioning_password" -A
+
+security list-keychains -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
